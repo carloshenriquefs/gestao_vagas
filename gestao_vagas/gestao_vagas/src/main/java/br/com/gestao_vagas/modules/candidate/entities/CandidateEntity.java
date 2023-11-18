f663087fc347c413084ef5c55a7bf3839c076cd6
Carlos Henrique <ichfs9108@gmail.com>
2023-11-18 01:00:12 -0300

    Refatorando Documentacao

--- gestao_vagas/gestao_vagas/src/main/java/br/com/gestao_vagas/modules/candidate/entities/CandidateEntity.java
+++ gestao_vagas/gestao_vagas/src/main/java/br/com/gestao_vagas/modules/candidate/entities/CandidateEntity.java
@@ -1,5 +1,6 @@
 package br.com.gestao_vagas.modules.candidate.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,17 +22,24 @@ public class CandidateEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Schema(example = "Daniel de Souza", requiredMode = Schema.RequiredMode.REQUIRED, description = "Nome do Candidato")
     private String name;
 
     @NotBlank
     @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
+    @Schema(example = "daniel", requiredMode = Schema.RequiredMode.REQUIRED, description = "Username do candidato")
     private String username;
 
     @Email(message = "O campo [email] deve conter um e-mail válido")
+    @Schema(example = "daniel@gmail.com", requiredMode = Schema.RequiredMode.REQUIRED, description = "E-mail do candidato")
     private String email;
 
     @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
+    @Schema(example = "admin@1234", minLength = 10, maxContains = 100, requiredMode = Schema.RequiredMode.REQUIRED, description = "Senha do candidato")
     private String password;
+
+    @Schema(example = "Desenvolvedor Java", requiredMode = Schema.RequiredMode.REQUIRED, description = "Breve descrição do candidato")
     private String description;
     private String curriculum;
 

88759fea3ba17d5ebebbff8981b268f7aa33d599
Carlos Henrique <ichfs9108@gmail.com>
2023-11-22 01:24:26 -0300

    Testando excecoes

---
+++ gestao_vagas/gestao_vagas/src/test/java/br/com/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -0,0 +1,37 @@
+package br.com.gestao_vagas.modules.candidate.useCases;
+
+import br.com.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+import br.com.gestao_vagas.modules.company.repositories.JobRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplyJobCandidateUseCaseTest {
+
+    @InjectMocks
+    private ApplyJobCandidateUseCase applyJobCandidateUseCase;
+
+    @Mock
+    private JobRepository jobRepository;
+
+    @Mock
+    private CandidateRepository candidateRepository;
+
+    @Test
+    @DisplayName("Should not be able to apply job with candidate not found")
+    public void should_not_be_able_to_apply_job_with_candidate_not_found() {
+        try {
+            applyJobCandidateUseCase.execute(null, null);
+        } catch(Exception e) {
+            assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+
+    }
+}

aba448d8cad25f94382aca4693ff39195b120ae6
Carlos Henrique <ichfs9108@gmail.com>
2024-01-15 23:09:27 -0300

    Qualidade de Código: Refatorando

--- gestao_vagas/gestao_vagas/src/main/java/br/com/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
+++ gestao_vagas/gestao_vagas/src/main/java/br/com/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -1,14 +1,12 @@
 package br.com.gestao_vagas.modules.candidate.useCases;
 
+import br.com.gestao_vagas.exceptions.UserNotFoundException;
 import br.com.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import br.com.gestao_vagas.modules.candidate.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-
-import static br.com.gestao_vagas.constants.Constants.USER_NOT_FOUND;
 
 @Service
 public class ProfileCandidateUseCase {
@@ -19,7 +17,7 @@ public class ProfileCandidateUseCase {
     public ProfileCandidateResponseDTO execute(UUID idCandidate) {
         var candidate = this.candidateRepository.findById(idCandidate)
                 .orElseThrow(() -> {
-                    throw new UsernameNotFoundException(USER_NOT_FOUND);
+                    throw new UserNotFoundException();
                 });
 
         var candidateDTO = ProfileCandidateResponseDTO.builder()

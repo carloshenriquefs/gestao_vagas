e85e5a05877a4c6c398e1cef5b28bf3f71c1fe8e
Carlos Henrique <ichfs9108@gmail.com>
2024-10-24 14:37:50 -0300

    feat: Backend Listagem

--- gestao_vagas/src/main/java/br/com/gestao_vagas/modules/company/usesCases/AuthCompanyUseCase.java
+++ gestao_vagas/src/main/java/br/com/gestao_vagas/modules/company/usesCases/AuthCompanyUseCase.java
@@ -54,9 +54,12 @@ public class AuthCompanyUseCase {
                 .withClaim("roles", Arrays.asList("COMPANY"))
                 .sign(algorithm);
 
+        var roles = Arrays.asList("COMPANY");
+
         var authCompanyResponseDTO = AuthCompanyResponseDTO.builder()
                 .access_token(token)
                 .expires_in(expireIn.toEpochMilli())
+                .roles(roles)
                 .build();
 
         return authCompanyResponseDTO;
---
+++ gestao_vagas/src/main/java/br/com/gestao_vagas/modules/company/usesCases/ListAllJobsByCompanyUseCase.java
@@ -0,0 +1,21 @@
+package br.com.gestao_vagas.modules.company.usesCases;
+
+import br.com.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.gestao_vagas.modules.company.repositories.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ListAllJobsByCompanyUseCase {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public List<JobEntity> execute(UUID companyId) {
+        return this.jobRepository.findByCompanyId(companyId);
+    }
+
+}
